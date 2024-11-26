6e51954ff2fe8699675ef01fac231f1907f21bcb
yoonju1030 <allrealjgt@gmail.com>
2024-11-26 22:56:54 +0900

    first

---
+++ src/main/java/mimmoo/mimmoo_back/MimmooBackApplication.java
@@ -0,0 +1,13 @@
+package mimmoo.mimmoo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimmooBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimmooBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mimmoo/mimmoo_back/MimmooBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mimmoo.mimmoo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimmooBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
